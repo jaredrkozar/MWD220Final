e204943b85c91deab942e433a46e9bde7ba09b2c
Jared Kozar <jared.rkozar@gmail.com>
2022-12-08 23:34:11 -0500

    added css

--- main.js
+++ main.js
@@ -2,39 +2,45 @@
 var consoles = [{consoleName: '', games: []}]
 var body = d3.select('body')
 
-d3.select("#nRadius").on("input", function() {
-    console.log(this.value)
+function selectYear(){
+    // Selecting the input element and get its value
+    var inputVal = document.getElementById("yearInput").value;
 
-    updateChart(this.value);
-});
+    if (1988 <= inputVal <= 2017) {
+        titleText.text("View games released in " + inputVal)
+        updateChart(inputVal);
+    } else {
+        alert("Enter a year between 1988 and 2017")
+    }
+}
 
-updateChart("1988");
+updateChart(1988);
 var margin = {top: 300, right: 20, bottom: 50, left: 300},
     width = 1200 - margin.left - margin.right,
     height = 1200 - margin.top - margin.bottom;
 
 var svg = body.append("svg")
     .attr("width", margin.left + margin.right + width + 300)
-    .attr("height", margin.top + margin.bottom + height)
+    .attr("height", margin.top + margin.bottom + height + 100)
     .append("g")
-    .attr("transform",
-        "translate(100,100)")
+
+var titleText = svg.append("text")
+    .text("View games released in 1988")
+    .attr("x", 500)
+    .attr("y", 20)
+    .attr("font-weight",600)
+    .attr("font-family", "Inter")
+    .style("font-size", "38px")
+    .attr("transform",  "translate(0,30)");
 
 var headerText = svg.append("text")
     .text("Select an area from the left to view the games released for that console this year")
     .attr("x", 620)
-    .attr("y", 20)
-    .attr("font-weight",900)
-    .attr("font-family", "Inter")
-    .style("font-size", "34px");
-
-var gamesText = svg.append("text")
-    .attr("x", 620)
-    .attr("y", 60)
-    .html(function (d) { return d.name; } + "<br />")
-    .attr("font-weight",900)
+    .attr("y", 50)
+    .attr("font-weight",600)
     .attr("font-family", "Inter")
     .style("font-size", "34px")
+    .attr("transform",  "translate(0,150)");
 
 var pie = d3.pie()
     .sort(null);
@@ -49,6 +55,7 @@ var textGroup = svg.append("g")
 var header = textGroup
 
 function updateChart(year) {
+    console.log("dld")
     consoles = [];
     d3.csv("/vgsales.csv").then(function(data) {
 
@@ -71,8 +78,9 @@ function updateChart(year) {
             .data(consoles)
         //select all arcs in the grou
 
+        console.log(consoles)
         var consoleLengths = [];
-        for(i=1;i<consoles.length;i++){
+        for(i=0;i<consoles.length;i++){
             consoleLengths.push(consoles[i].games.length)
         }
 
@@ -82,13 +90,14 @@ function updateChart(year) {
             .enter()
 
         arcs.append("path")
+            .attr("transform",  "translate(0,150)")
             .attr("fill", (data, i)=>{
                 return d3.schemeSet3[i];
             })
             .attr("d", arc)
             .on("mousedown", (data, i)=>{
-                gamesText.html("DLLDLD \r\n kekekek")
-                headerText.text("There were " + consoles[i.index + 1].games.length + " " + consoles[i.index + 1].consoleName + " games released in " + year)
+                document.getElementById("gamesText").innerHTML = consoles[i.index].games.join("<br>")
+                headerText.text("There were " + consoles[i.index].games.length + " " + consoles[i.index].consoleName + " games released in " + year)
             })
     })
 
